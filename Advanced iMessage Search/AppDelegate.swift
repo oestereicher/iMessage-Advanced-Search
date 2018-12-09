dd54dd1e02d79262251a7eae0e40649cbbccae3a
oestereicher <brandon.oestereicher@gmail.com>
2018-12-09 15:29:07 -0800

    Initial Commit

---
+++ Advanced iMessage Search/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Advanced iMessage Search
+//
+//  Created by Brandon Oestereicher on 12/9/18.
+//  Copyright © 2018 Brandon Oestereicher. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Advanced iMessage Search/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Advanced iMessage Search
+//
+//  Created by Brandon Oestereicher on 12/9/18.
+//  Copyright © 2018 Brandon Oestereicher. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
